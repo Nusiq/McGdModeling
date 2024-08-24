0b76be1e4a626b70f0fa04023d1f8f922a69b3c7
Nusiq <nusiq@outlook.com>
2024-08-24 02:06:08 +0200

    Implemented selection outline scene:
    The scene renders outlines using additional SubViewport and camera. It uses 2 shaders - 1 on a mesh covering the view and 1 on the SubViewportContainer.
    - The first shader creates a map on the red color channel image with edges highlighted and non edges set to 0.
    - The second shader uses that map to render the outline on the screen or to change its color to fully transparent.
    
    Outlines are visible through the wall.
    
    The outline system allows setting view masks to mark which objects should be outlined. Warning - Setting it to the mask visible by the main camera will cover the screen with the image from the first shader.

---
+++ 3d/selection_outline/selection_outline.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@export var outline_color: Color = Color(1.0, 0.627, 0.157, 1.0):
+	get:
+		return outline_color
+	set(value):
+		outline_color = value
+		var viewport_container: SubViewportContainer = $SubViewportContainer
+		if viewport_container:
+			viewport_container.material["shader_parameter/outlineColor"] = value
+
+
+@export_flags_3d_render var outline_layers: int = 2:
+	get:
+		return outline_layers
+	set(value):
+		outline_layers = value
+		var posprocessing_mesh: MeshInstance3D = $MeshInstance3D
+		var camera: Camera3D = $SubViewportContainer/SubViewport/Camera3D
+		if posprocessing_mesh and camera:
+			posprocessing_mesh.layers = value
+			camera.cull_mask = value
